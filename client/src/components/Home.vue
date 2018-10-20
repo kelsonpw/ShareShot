f3a4d043cef011f0af638e0f3a86bfd9438af5c5
Kelson Warner <kelsonpw35@gmail.com>
2018-10-20 14:34:43 -0700

    wired up apollo client

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -2,13 +2,36 @@
   <div>
     <v-container>
       <h1>Home</h1>
+      <ul v-for="post in getPosts" :key="post._id">
+        <li>{{post.title}}
+          {{post.imageUrl}}
+          {{post.description}}
+        </li>
+        <li>{{post.likes}}</li>
+      </ul>
     </v-container>
   </div>
 
 </template>
 
 <script>
+import { gql } from 'apollo-boost';
 export default {
-  name: 'home'
+  name: 'home',
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            description
+            likes
+          }
+        }
+      `
+    }
+  }
 };
 </script>
